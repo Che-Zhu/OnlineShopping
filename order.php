7b7a465d8e4ebce8a4bfb89223a34d667ff171a4
Cheaww <30403707+Cheaww@users.noreply.github.com>
2018-09-27 17:07:34 +1000

    Connection with database
    Establish connection with database

--- order.php
+++ order.php
@@ -1,1 +1,17 @@
 <?php session_start() ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Online Shopping</title>
+</head>
+<body background="background.jpg">
+	<?php 
+		$conn = mysqli_connect('localhost','root', '', 'pizzastore');
+		if ( !$conn ) {
+				die("Connection failed: " .mysqli_connect_error());      /* Establish connection with database */
+		} 
+	?>
+
+</body>
+</html>
