8ce5dd5d77e9d36036a5651d43583037d422dc67
Cheaww <30403707+Cheaww@users.noreply.github.com>
2018-09-27 18:33:07 +1000

    order php build
    h1 with color
    order.php with pictures

--- order.php
+++ order.php
@@ -4,6 +4,7 @@
 <html>
 <head>
 	<title>Online Shopping</title>
+	<link rel="stylesheet" type="text/css" href="mystyle.css">
 </head>
 <body background="background.jpg">
 	<?php 
@@ -11,7 +12,25 @@
 		if ( !$conn ) {
 				die("Connection failed: " .mysqli_connect_error());      /* Establish connection with database */
 		} 
+
+
+		$sql = "SELECT * FROM pizza";
+		$pizzaDetail = mysqli_query($conn, $sql) 
+		or die('Problem with query' . mysqli_error());       /* Get the fourseason info from database */
 	?>
 
+	<h1>Order a pizza now</h1>
+	<form method="post" action="sause.php">
+		
+	<?php 
+		while ($row = mysqli_fetch_array($pizzaDetail, MYSQLI_ASSOC)) { /* Display pictures, name of pictures was retrieved from database */
+	?>
+	<img src=<?php echo $row["Picture"] ?>>                     
+
+	</form>
+
+	<?php
+		}
+	?>
 </body>
 </html>
