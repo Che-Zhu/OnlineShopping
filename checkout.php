8548cd72751daf59c36b157688d867981c5aa09a
Cheaww <30403707+Cheaww@users.noreply.github.com>
2018-09-30 21:27:55 +1000

    checkout.php
    size info dev
    price info dev
    information check

--- checkout.php
+++ checkout.php
@@ -17,11 +17,24 @@
 
 
 
+
 		for ($i=0; $i < count($_SESSION['pizzaSession']); $i++) { 
 			echo "<img src=".$_SESSION['pizzaSession'][$i].">";
 
+			$sizeTemp = $_SESSION['pizzaSession'][$i];
+
+			$sqlSize = "SELECT Size, Price FROM pizza WHERE Picture = '$sizeTemp'";
+			$size = mysqli_query($conn, $sqlSize);
+			$row = mysqli_fetch_array($size, MYSQLI_ASSOC);
+			echo "<span>The size of the pizza is: </span>";
+			echo $row["Size"];
+			echo "<span>The price of the pizza is: </span>";
+			echo $row ["Price"];
+
+
+
 			echo "<img src=".$_SESSION['sauce'][$i].">";
-			echo 
+
 			echo "<br>";
 		}
 		?>
@@ -32,10 +45,17 @@
 
 
 
+
+
 	<h1>Please enter the required information to process your order:</h1>
 
 
-	<form method="post" action="confirm.php">
+
+
+
+
+
+	<form method="post" action="<?php echo $_SERVER["PHP_SELF"];?>" >
 		<label for="firstName">First Name</label>
 		<input type="text" name="FirstName" id="firstName"> <br>
 		<label for="lastName">Last Name</label>
@@ -44,5 +64,28 @@
 		<input type="email" name="Email" id="email"> <br>
 		<input type="submit" name="submit" value="Confirm">
 	</form>
+
+	<?php
+		// define variables and set to empty values
+		$firstName = $lastName = $firstf = $firstff = $lastf = $laftff = "";
+
+		if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  			$firstName = $_POST["FirstName"];
+			$lastName = $_POST["LastName"];
+		
+			$checkFirst = "SELECT GivenName FROM customer WHERE GivenName = '$firstName'";
+			$checkLast = "SELECT GivenName FROM customer WHERE LastName = '$lastName'";
+			$firstf = mysqli_query($conn, $checkFirst);
+			$lastf = mysqli_query($conn, $checkLast);
+			$firstff = mysqli_fetch_array($firstf, MYSQLI_ASSOC);
+			$lastff = mysqli_fetch_array($lastf, MYSQLI_ASSOC);
+			if ($firstName == NULL || $firstff == NULL || $lastName == NULL || $lastff == NULL) {
+				echo '<p>The information you provided does not match with our database!</p>';
+				return false;
+			}
+		}
+
+
+?>
 
 </html>
